444fc1a1a3f6873de73c8a8be1124b4ae0d7e1a0
Isaac <isaac231467@gmail.com>
2022-09-04 23:00:31 -0500

    added a few ui items to the view I also added a small functionality to the login and signup buttons

--- companion-app/ViewController.swift
+++ companion-app/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.setTitle("Welcome back! 🥳", for: .normal)
     }
 
-
+    @IBAction func signupPressed(_ sender: UIButton) {
+        sender.setTitle("Better late than never! 😜", for: .normal)
+    }
+    
 }
 
